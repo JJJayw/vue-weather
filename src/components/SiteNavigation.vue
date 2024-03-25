da78680a4c15d5ae3825e551976efc35fc41d8d4
JJJayw <271753416@qq.com>
2024-03-25 23:54:40 +0800

    完成搜索城市搜索功能

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,61 +1,56 @@
 <template>
-    <header class="sticky top-0 bg-weather-primary shadow-lg">
-        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
-            <RouterLink :to="{ name: 'home' }">
-                <div class="flex items-center gap-3">
-                    <i class="fa-solid fa-sun text-2xl"></i>
-                    <p class="text-2xl">The Local Weather</p>
-                </div>
-            </RouterLink>
-            <div class="flex gap-3 flex-1 justify-end">
-                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer" @click="toggleModal"></i>
-                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
-            </div>
+	<header class="sticky top-0 bg-weather-primary shadow-lg">
+		<nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+			<RouterLink :to="{ name: 'home' }">
+				<div class="flex items-center gap-3">
+					<i class="fa-solid fa-sun text-2xl"></i>
+					<p class="text-2xl">天气预报</p>
+				</div>
+			</RouterLink>
+			<div class="flex gap-3 flex-1 justify-end">
+				<i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+				   @click="toggleModal"></i>
+				<i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+			</div>
 
-            <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
-                <div class="text-black">
-                    <h1 class="text-2xl mb-1">About:</h1>
-                    <p class="mb-4">
-                        The Local Weather allows you to track the current and
-                        future weather of cities of your choosing.
-                    </p>
-                    <h2 class="text-2xl">How it works:</h2>
-                    <ol class="list-decimal list-inside mb-4">
-                        <li>
-                            Search for your city by entering the name into the
-                            search bar.
-                        </li>
-                        <li>
-                            Select a city within the results, this will take
-                            you to the current weather for your selection.
-                        </li>
-                        <li>
-                            Track the city by clicking on the "+" icon in the
-                            top right. This will save the city to view at a
-                            later time on the home page.
-                        </li>
-                    </ol>
+			<BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+				<div class="text-black">
+					<h1 class="text-2xl mb-1 py-2">关于:</h1>
+					<p class="mb-4">
+						本地天气允许您跟踪当前和您选择的城市的未来天气。
+					</p>
+					<h2 class="text-2xl py-2">如何使用</h2>
+					<ol class="list-decimal list-inside mb-4">
+						<li>
+							通过在搜索栏中输入名称来搜索您的城市。
+						</li>
+						<li>
+							在结果中选择一个城市，这将带您到当前天气进行选择。
+						</li>
+						<li>
+							单击右上角的“+”图标来跟踪城市。这将保存城市，以便以后在主页上查看。
+						</li>
+					</ol>
 
-                    <h2 class="text-2xl">Removing a city</h2>
-                    <p>
-                        If you no longer wish to track a city, simply select
-                        the city within the home page. At the bottom of the
-                        page, there will be am option to delete the city.
-                    </p>
-                </div>
-            </BaseModal>
-        </nav>
+					<h2 class="text-2xl py-2">删除城市</h2>
+					<p>
+						如果您不再希望跟踪某个城市，只需在主页中选择该城市即可。在页面底部，将有一个删除城市的选项。
+					</p>
+				</div>
+			</BaseModal>
+		</nav>
 
-    </header>
+	</header>
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from "vue-router";
-import BaseModal  from "@/components/BaseModal.vue";
+import {RouterLink} from "vue-router";
+import BaseModal from "@/components/BaseModal.vue";
 import {ref} from "vue";
 
+
 const modalActive = ref(false);
-const toggleModal = ()=>{
-    modalActive.value = !modalActive.value;
+const toggleModal = () => {
+	modalActive.value = !modalActive.value;
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,18 @@
 			<input type="text"
 			       v-model="searchQuery"
 			       @input="getSearchResults"
-			       placeholder="Search for a city or state"
+			       placeholder="搜索城市"
 			       class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
-			<ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]">
-				<li v-for="searchResult in  mapSearchResults" :key="searchResult.id"
+
+			<ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+			    v-if="mapSearchResults">
+				<li v-if="searchError" class="py-2 list-none">抱歉，</li>
+				<li v-if="!searchError && mapSearchResults.length === 0" class="py-2 list-none">
+					没有您查询的结果，请重新输入
+				</li>
+				<li v-for="searchResult in  mapSearchResults" :key="searchResult.adcode"
 				    class="py-2 cursor-pointer">
-					{{ searchResult.place_name }}
+					{{ searchResult.name }}
 				</li>
 			</ul>
 
@@ -20,25 +26,41 @@
 <script setup lang="ts" name="HomeView">
 import {ref} from "vue";
 import axios from "axios";
+import * as data from "@/resource/api.json";
 
 // API密钥
-const APIKey = null;
-
+const APIKey = data.apiKey;
+// 查询关键字
 const searchQuery = ref("");
-
+// 懒搜索 时间
 const queryTimeout = ref();
-
+// 查询结果集
 const mapSearchResults: any = ref();
+
+const searchError = ref();
 
 const getSearchResults = () => {
 	clearTimeout(queryTimeout.value);
 	queryTimeout.value = setTimeout(async () => {
 		if (searchQuery.value !== "") {
-			const result = await axios.get(
-				// API URL
-				``
-			);
-			mapSearchResults.value = result.data.features;
+			try {
+				const result: any = await axios.get(
+					// API URL
+					"https://restapi.amap.com/v3/config/district",
+					{
+						params: {
+							keywords: searchQuery.value,
+							subdistrict: 0,
+							key: APIKey,
+							extensions: "base",
+							offset: 10
+						}
+					}
+				);
+				mapSearchResults.value = result.data.districts;
+			} catch {
+				searchError.value = true
+			}
 			return
 		}
 		mapSearchResults.value = null;
