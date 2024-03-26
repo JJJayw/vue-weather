6e111fc0d50530e724acd2760fe4beeb36377bd5
JJJayw <271753416@qq.com>
2024-03-26 09:22:08 +0800

    修改字体样式

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+	<div class="flex flex-col min-h-screen font-yaHei  bg-weather-primary">
 		<SiteNavigation></SiteNavigation>
 		<router-view></router-view>
 	</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "weather-primary": "#00668A",
-        "weather-secondary": "#004E71",
-      },
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "weather-primary": "#00668A",
+                "weather-secondary": "#004E71",
+            },
+        },
+        fontFamily: {
+            yaHei: "Microsoft YaHei, 微软雅黑, Arial",
+        },
+        container: {
+            padding: "2rem",
+            center: true,
+        },
+        screens: {
+            sm: "640px",
+            md: "768px",
+        }
     },
-    fontFamily: {
-      Roboto: ["Roboto, sans-serif"],
-    },
-    container: {
-      padding: "2rem",
-      center: true,
-    },
-    screens: {
-      sm: "640px",
-      md: "768px",
-    }
-  },
-  plugins: [],
+    plugins: [],
 }
